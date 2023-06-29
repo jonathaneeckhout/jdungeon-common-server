a44a95d9ad633ca62c63a21882a2ef0bc9ebe821
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-06-29 21:30:11 +0200

    password should not be unique

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ const createPlayersTableQuery = `
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL UNIQUE
+    password VARCHAR(255) NOT NULL
   );
 `;
 
