78d8d759982c5a163ae74d49a5e02c359a9fa0fe
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-07-16 23:19:37 +0200

    retry when connection failed

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 // Load the .env file
 require('dotenv').config();
 
-// Init the database
-require("./src/initconfig").run();
+var utils = require("./src/utils");
 
-// Start the app server
-var appHandler = require("./src/apphandler").getInstance();
-var wsHandler = require("./src/wshandler").getInstance();
-appHandler.run();
+// Init the database
+var Database = require("./src/database");
+var database = Database.getInstance();
+
+var initConfig = require("./src/initconfig")
+
+const RETRY_TIME = 3000;
+
+async function run() {
+    while (! await database.init()) {
+        console.log(`Could not init database, retrying in ${RETRY_TIME} ms`);
+        await utils.delay(RETRY_TIME);
+    }
+    console.log("database connected");
+
+    // Init the database values
+    initConfig.run();
+
+    // Start the app server
+    var appHandler = require("./src/apphandler").getInstance();
+    var wsHandler = require("./src/wshandler").getInstance();
+    appHandler.run();
+}
+
+
+run();
--- src/database.js
+++ src/database.js
@@ -39,10 +39,9 @@ const createCharactersTableQuery = `
 
 class Database {
     constructor() {
+        this.online = false;
         this._instance = null;
         this._pool = null;
-
-        this.init();
     }
 
     static getInstance() {
@@ -52,7 +51,7 @@ class Database {
         return this._instance;
     }
 
-    init() {
+    async init() {
         // Connect to database
         this._pool = new Pool({
             user: DB_USER,
@@ -63,38 +62,45 @@ class Database {
         });
 
         // Create level table
-        this._pool.query(createLevelsTableQuery, (err, result) => {
-            if (err) {
-                console.error('Error creating level table:', err);
-            } else {
-                console.log('Levels table created successfully');
-            }
-        });
+        try {
+            await this._pool.query(createLevelsTableQuery);
+        } catch (error) {
+            console.error('Error creating level table:', error);
+            return false;
+        }
+
+        console.log('Levels table created successfully');
 
         // Create players table
-        this._pool.query(createPlayersTableQuery, (err, result) => {
-            if (err) {
-                console.error('Error creating players table:', err);
-            } else {
-                console.log('Players table created successfully');
-            }
-        });
+        try {
+            await this._pool.query(createPlayersTableQuery);
+        } catch (error) {
+            console.error('Error creating players table:', error);
+            return false;
+        }
+
+        console.log('Players table created successfully');
 
         // Create characters table
-        this._pool.query(createCharactersTableQuery, (err, result) => {
-            if (err) {
-                console.error('Error creating characters table:', err);
-            } else {
-                console.log('Characters table created successfully');
-            }
-        });
+        try {
+            await this._pool.query(createCharactersTableQuery);
+        } catch (error) {
+            console.error('Error creating characters table:', error);
+            return false;
+        }
+
+        console.log('Characters table created successfully');
+
+        this.online = true;
+        return true;
     }
 
     async auth_player(username, password) {
-        var err, result = await this._pool.query('SELECT * FROM players WHERE username = $1', [username]);
-        if (err) {
-            console.error('Error executing query', err);
-            return err, false;
+        try {
+            var result = await this._pool.query('SELECT * FROM players WHERE username = $1', [username]);
+        } catch (error) {
+            console.error('Error executing query', error);
+            return error, false;
         }
 
         if (result.rowCount == 0) {
@@ -106,61 +112,79 @@ class Database {
             // Compare the provided password with the hashed password
             const isMatch = await bcrypt.compare(password, result.rows[0]["password"]);
             return null, isMatch;
-        } catch (err) {
-            console.error('Error comparing passwords:', err);
+        } catch (error) {
+            console.error('Error comparing passwords:', error);
             return err, false;
         }
 
     }
 
     async auth_level(level, key) {
-        var err, result = await this._pool.query('SELECT * FROM levels WHERE level = $1 AND key = $2', [level, key]);
-        if (err) {
-            console.error('Error executing query', err);
-            return err, false;
+        try {
+            var result = await this._pool.query('SELECT * FROM levels WHERE level = $1 AND key = $2', [level, key]);
+        } catch (error) {
+            console.error('Error executing query', error);
+            return error, false;
         }
 
         return null, (result.rowCount > 0);
     }
 
     async create_character(player, character, level, position) {
-        var err, _ = await this._pool.query(
-            'INSERT INTO characters (name, player, level, pos_x, pos_y) VALUES ($1, $2, $3, $4, $5)',
-            [player, character, level, position.x, position.y]
-        );
-        return err;
+        try {
+            await this._pool.query(
+                'INSERT INTO characters (name, player, level, pos_x, pos_y) VALUES ($1, $2, $3, $4, $5)',
+                [player, character, level, position.x, position.y]
+            );
+        } catch (error) {
+            console.error('Error executing query', error);
+            return error;
+        }
     }
 
     async get_character(character_name) {
-        var err, result = await this._pool.query('SELECT * FROM characters WHERE name = $1', [character_name]);
-        if (err) {
-            console.error('Error executing query', err);
-            return err, null;
+        try {
+            var result = await this._pool.query('SELECT * FROM characters WHERE name = $1', [character_name]);
+        } catch (error) {
+            console.error('Error executing query', error);
+            return error, null;
         }
-
         return null, (result.rowCount > 0) ? result.rows[0] : null;
     }
 
     async get_level(level_name) {
-        var err, result = await this._pool.query('SELECT * FROM levels WHERE level = $1', [level_name]);
-        if (err) {
-            console.error('Error executing query', err);
-            return err, null;
+        try {
+            var result = await this._pool.query('SELECT * FROM levels WHERE level = $1', [level_name]);
+        } catch (error) {
+            console.error('Error executing query', error);
+            return error, null;
         }
 
         return null, (result.rowCount > 0) ? result.rows[0] : null;
     }
 
     async clear_levels() {
-        await this._pool.query('DELETE FROM levels;');
+        try {
+            await this._pool.query('DELETE FROM levels;');
+        } catch (error) {
+            console.error('Error executing query', error);
+            return false;
+        }
+        return true;
     }
 
     async create_level(level, key, address, port) {
-        var err, _ = await this._pool.query(
-            'INSERT INTO levels (level, key, address, port) VALUES ($1, $2, $3, $4)',
-            [level, key, address, port]
-        );
-        return err;
+        try {
+            await this._pool.query(
+                'INSERT INTO levels (level, key, address, port) VALUES ($1, $2, $3, $4)',
+                [level, key, address, port]
+            );
+
+        } catch (error) {
+            console.error('Error executing query', error);
+            return false;
+        }
+        return true;
     }
 }
 
